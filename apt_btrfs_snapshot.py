5056365b39117f53a4b3c3e6df38953b31710888
Kyle Agronick <agronick@gmail.com>
2015-06-02 20:49:50 -0400

    Working program using snapper entirely

--- apt_btrfs_snapshot.py
+++ apt_btrfs_snapshot.py
@@ -37,163 +37,110 @@ class AptBtrfsNotSupportedError(AptBtrfsSnapshotError):
 class AptBtrfsRootWithNoatimeError(AptBtrfsSnapshotError):
     pass
 
-
-class FstabEntry(object):
-    """ a single fstab entry line """
-    @classmethod
-    def from_line(cls, line):
-        # split up
-        args = line.partition("#")[0].split()
-        # use only the first 7 args and ignore anything after them, mount
-        # seems to do the same, see bug #873411 comment #7
-        return FstabEntry(*args[0:6])
-
-    def __init__(self, fs_spec, mountpoint, fstype, options, dump=0, passno=0):
-        # uuid or device
-        self.fs_spec = fs_spec
-        self.mountpoint = mountpoint
-        self.fstype = fstype
-        self.options = options
-        self.dump = dump
-        self.passno = passno
-
-    def __repr__(self):
-        return "<FstabEntry '%s' '%s' '%s' '%s' '%s' '%s'>" % (
-            self.fs_spec, self.mountpoint, self.fstype,
-            self.options, self.dump, self.passno)
-
-
-class Fstab(list):
-    """ a list of FstabEntry items """
-    def __init__(self, fstab="/etc/fstab"):
-        super(Fstab, self).__init__()
-
-        with open(fstab) as fstab_file:
-            for line in (l.strip() for l in fstab_file):
-                if line == "" or line.startswith("#"):
-                    continue
-                try:
-                    entry = FstabEntry.from_line(line)
-                except ValueError:
-                    continue
-                self.append(entry)
+ 
 
 
 class LowLevelCommands(object):
     """ lowlevel commands invoked to perform various tasks like
         interact with mount and btrfs tools
-    """
-    def mount(self, fs_spec, mountpoint):
-        ret = subprocess.call(["mount", fs_spec, mountpoint])
+    """ 
+    
+    def list(self):
+        ret = subprocess.check_output(["snapper", "list"], universal_newlines=True);
+        ret = ret.split("\n")
+        i = 0; 
+        items = []
+        for item in ret: 
+            i=1+i 
+            if i < 3:
+                continue;
+            split = item.split("|");
+            if len(split) > 4: 
+                row = {
+                    'name' : split[6].strip(),
+                    'id' : split[1].strip(),
+                    'cleanup' : split[5].strip(),
+                    'user_data' : split[7].strip(),
+                    'user' : split[4].strip(),
+                    'date' : split[3].strip(),
+                    'text' : item
+                } 
+                if row['name'].startswith(AptBtrfsSnapshot.SNAP_PREFIX):
+                    items.append(row) 
+        return items;
+                    
+        
+
+    def btrfs_subvolume_snapshot(self, description):
+        ret = subprocess.call(["snapper", "create", "-d", description, "-c", "timeline"])
         return ret == 0
 
-    def umount(self, mountpoint):
-        ret = subprocess.call(["umount", mountpoint])
+    def btrfs_delete_snapshot(self, id):
+        ret = subprocess.call(["snapper", "delete", id]) 
         return ret == 0
-
-    def btrfs_subvolume_snapshot(self, source, dest):
-        ret = subprocess.call(["btrfs", "subvolume", "snapshot",
-                               source, dest])
-        return ret == 0
-
-    def btrfs_delete_snapshot(self, snapshot):
-        ret = subprocess.call(["btrfs", "subvolume", "delete", snapshot])
-        return ret == 0
-
+    
+    def btrfs_restore_snapshot(self, id):
+        ret = subprocess.call(["snapper", "undochange", id+"..0"])
+        return ret == 0;
+    
+    def btrfs_snapshot_diff(self, id):
+        ret = subprocess.check_output(["snapper", "status", id+"..0" ], universal_newlines=True);
+        return ret
 
 class AptBtrfsSnapshot(object):
     """ the high level object that interacts with the snapshot system """
 
     # normal snapshot
-    SNAP_PREFIX = "@apt-snapshot-"
+    SNAP_PREFIX = "apt-snapper-"
+    SNAPPER_TIME = "%a %d %b %Y %I:%M:%S %p %Z"
     # backname when changing
-    BACKUP_PREFIX = SNAP_PREFIX+"old-root-"
+    BACKUP_PREFIX = SNAP_PREFIX + "old-root-"
 
-    def __init__(self, fstab="/etc/fstab"):
-        self.fstab = Fstab(fstab)
-        self.commands = LowLevelCommands()
-        self._btrfs_root_mountpoint = None
+    def __init__(self): 
+        self.commands = LowLevelCommands() 
 
     def snapshots_supported(self):
         """ verify that the system supports apt btrfs snapshots
             by checking if the right fs layout is used etc
         """
         # check for the helper binary
-        if not os.path.exists("/sbin/btrfs"):
-            return False
-        # check the fstab
-        entry = self._get_supported_btrfs_root_fstab_entry()
-        return entry != None
-
-    def _get_supported_btrfs_root_fstab_entry(self):
-        """ return the supported btrfs root FstabEntry or None """
-        for entry in self.fstab:
-            if (entry.mountpoint == "/" and
-                entry.fstype == "btrfs" and
-                "subvol=@" in entry.options):
-                return entry
-        return None
-
-    def _uuid_for_mountpoint(self, mountpoint, fstab="/etc/fstab"):
-        """ return the device or UUID for the given mountpoint """
-        for entry in self.fstab:
-            if entry.mountpoint == mountpoint:
-                return entry.fs_spec
-        return None
-
-    def mount_btrfs_root_volume(self):
-        uuid = self._uuid_for_mountpoint("/")
-        mountpoint = tempfile.mkdtemp(prefix="apt-btrfs-snapshot-mp-")
-        if not self.commands.mount(uuid, mountpoint):
-            return None
-        self._btrfs_root_mountpoint = mountpoint
-        return self._btrfs_root_mountpoint
-
-    def umount_btrfs_root_volume(self):
-        res = self.commands.umount(self._btrfs_root_mountpoint)
-        os.rmdir(self._btrfs_root_mountpoint)
-        self._btrfs_root_mountpoint = None
-        return res
+        return os.path.exists("/usr/bin/snapper")
+  
 
     def _get_now_str(self):
-        return  datetime.datetime.now().replace(microsecond=0).isoformat(str('_'))
+        return datetime.datetime.now().replace(microsecond=0).isoformat(
+            str('_'))
 
-    def create_btrfs_root_snapshot(self, additional_prefix=""):
-        mp = self.mount_btrfs_root_volume()
+    def create_btrfs_root_snapshot(self, additional_prefix=""): 
         snap_id = self._get_now_str()
-        res = self.commands.btrfs_subvolume_snapshot(
-            os.path.join(mp, "@"),
-            os.path.join(mp, self.SNAP_PREFIX+additional_prefix+snap_id))
-        self.umount_btrfs_root_volume()
+        name = self.SNAP_PREFIX + additional_prefix + snap_id;
+        res = self.commands.btrfs_subvolume_snapshot(name)
+        print("Created snapshot with snapper named " + name)
         return res
 
-    def get_btrfs_root_snapshots_list(self, older_than=0):
+    def get_btrfs_root_snapshots_list(self, older_than=0, only_id=False):
         """ get the list of available snapshot
             If "older_then" is given (in unixtime format) it will only include
             snapshots that are older then the given date)
         """
         l = []
-        # if older_than is used, ensure that the rootfs does not use
-        # "noatime"
-        if older_than != 0:
-            entry = self._get_supported_btrfs_root_fstab_entry()
-            if not entry:
-                raise AptBtrfsNotSupportedError()
-            if "noatime" in entry.options:
-                raise AptBtrfsRootWithNoatimeError()
-        # if there is no older than, interpret that as "now"
         if older_than == 0:
             older_than = time.time()
-        mp = self.mount_btrfs_root_volume()
-        for e in os.listdir(mp):
-            if e.startswith(self.SNAP_PREFIX):
-                # fstab is read when it was booted and when a snapshot is
-                # created (to check if there is support for btrfs)
-                atime = os.path.getatime(os.path.join(mp, e, "etc", "fstab"))
-                if atime < older_than:
-                    l.append(e)
-        self.umount_btrfs_root_volume()
-        return l
+        
+        items = self.commands.list()
+        for item in items:
+            mtime = time.mktime(time.strptime(item['date'], self.SNAPPER_TIME));
+            if( older_than > mtime ):
+                l.append( item['id'] if only_id else item)
+        
+        if only_id:
+            return l
+        
+        items = []
+        for item in l:
+            items.append(item['text'])
+        
+        return items
 
     def print_btrfs_root_snapshots(self):
         print("Available snapshots:")
@@ -212,9 +159,10 @@ class AptBtrfsSnapshot(object):
         try:
             print("Available snapshots older than '%s':" % timefmt)
             print("  \n".join(self.get_btrfs_root_snapshots_list(
-                    older_than=older_than_unixtime)))
+                older_than=older_than_unixtime)))
         except AptBtrfsRootWithNoatimeError:
-            sys.stderr.write("Error: fstab option 'noatime' incompatible with option")
+            sys.stderr.write("Error: fstab option 'noatime' incompatible "
+                             "with option \n")
             return False
         return True
 
@@ -223,35 +171,54 @@ class AptBtrfsSnapshot(object):
         older_than_unixtime = self._parse_older_than_to_unixtime(timefmt)
         try:
             for snap in self.get_btrfs_root_snapshots_list(
-                older_than=older_than_unixtime):
+                    older_than=older_than_unixtime, only_id=True):
                 res &= self.delete_snapshot(snap)
         except AptBtrfsRootWithNoatimeError:
-            sys.stderr.write("Error: fstab option 'noatime' incompatible with option")
+            sys.stderr.write("Error: fstab option 'noatime' incompatible with "
+                             "option \n")
             return False
         return res
+    
+    def show_diff(self, snapshot):
+        id = self.convert_name_to_id(snapshot)
+        
+        if id != -1:
+            diff = self.commands.btrfs_snapshot_diff(id)
+            print(diff)
+            return True
+        return False
+    
 
     def command_set_default(self, snapshot_name):
         res = self.set_default(snapshot_name)
         return res
+    
+    def convert_name_to_id(self, snapshot_name):
+        id=-1
+        snapshot_name = snapshot_name.strip();
+        try: 
+            int(snapshot_name)
+            id=snapshot_name
+        except ValueError:  
+            list = self.commands.list();
+            for item in list:
+                if item['name'].strip() == snapshot_name:
+                    id=item['id']
+                    
+        if id == -1:
+            sys.stderr.write("Could not find a snapshot with the supplied name or id \n")
+                    
+        return id;
 
     def set_default(self, snapshot_name, backup=True):
         """ set new default """
-        mp = self.mount_btrfs_root_volume()
-        new_root = os.path.join(mp, snapshot_name)
-        if os.path.isdir(new_root) and snapshot_name.startswith("@") and snapshot_name != "@":
-            default_root = os.path.join(mp, "@")
-            backup = os.path.join(mp, self.BACKUP_PREFIX + self._get_now_str())
-            os.rename(default_root, backup)
-            os.rename(new_root, default_root)
-            print("Default changed to %s, please reboot for changes to take effect." % snapshot_name)
-        else:
-            print("You have selected an invalid snapshot. Please make sure that it exists, and that it is not \"@\".")
-        self.umount_btrfs_root_volume()
-        return True
+        id = self.convert_name_to_id(snapshot_name)
+        
+        if id != -1:
+            ret = self.commands.btrfs_restore_snapshot(id) 
+            return ret
+        return False      
 
-    def delete_snapshot(self, snapshot_name):
-        mp = self.mount_btrfs_root_volume()
-        res = self.commands.btrfs_delete_snapshot(
-            os.path.join(mp, snapshot_name))
-        self.umount_btrfs_root_volume()
+    def delete_snapshot(self, snapshot_name): 
+        res = self.commands.btrfs_delete_snapshot(snapshot_name)
         return res
